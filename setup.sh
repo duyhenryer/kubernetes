96e59b30d77b8c4e257dd21931ecd432e968fed1
Duy Do <duyhenry250897@gmail.com>
2020-07-10 10:34:29 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+command=$1
+
+case "$command" in 
+
+     "up" )
+	echo "master up"
+	echo "--------------------"
+	cd master.xtl && vagrant up
+	cd ..
+
+	echo "worker-1 up"
+	echo "--------------------"
+	cd worker1.xtl && vagrant up
+	cd ..
+
+	echo "worker-2 up"
+	echo "--------------------"
+	cd worker2.xtl && vagrant up
+	
+	echo "vagrant up done"
+
+     "reload" )
+	echo "master reload"
+	echo "--------------------"
+	cd master.xtl && vagrant reload
+	cd ..
+
+	echo "worker-1 reload"
+	echo "--------------------"
+	cd worker1.xtl && vagrant reload
+	cd ..
+
+	echo "worker-2 reload"
+	echo "--------------------"
+	cd worker2.xtl && vagrant reload
+
+	echo "--------------------"
+	echo "vagrant reload done"
+
+
+
